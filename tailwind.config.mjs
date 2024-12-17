e751b0cf476920d6a0f88ca11c06e71d702ad8de
Mike Batelahoko <mikebatelahoko96@gmail.com>
2024-12-17 15:47:03 +0100

    I personnalized color in tailwind.config + used colors in welcome.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,14 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			formBg: 'hsl(0, 0%, 100%)', 
+			customGreen: 'hsl(148, 38%, 91%)',
+			customAnotherGreen: 'hsl(169, 82%, 27%)',
+			customTheGreen: 'hsl(186, 15%, 59%)',
+			customDarkGreen: 'hsl(187, 24%, 22%)',
+			errorMessage: 'hsl(0, 66%, 54%)',
+		},
 	},
 	plugins: [],
 }
